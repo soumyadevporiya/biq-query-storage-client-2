9e9e56db462a5917599fc9051d372fcc8f72ce9a
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-04-01 14:11:22 +0530

    Table name changed

--- bq_storage_read_api_v4.py
+++ bq_storage_read_api_v4.py
@@ -30,7 +30,7 @@ read_options = ReadSession.TableReadOptions(
               selected_fields=["id", "name"]
     )
 #read_options.row_restriction = "partition_field like '%INSBI1%'"
-read_options.row_restriction = "partition_field BETWEEN 4 AND 5"
+read_options.row_restriction = "partition_field BETWEEN 6 AND 7"
 
 parent = "projects/{}".format(project_id_billing)
 
